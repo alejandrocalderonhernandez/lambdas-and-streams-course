340f8e4aac44844ab81012eb4afada55fbeb4467
Alejandro <alejandro950825@gmail.com>
2022-09-13 20:24:52 -0500

    add map nad flatmap example

--- src/main/java/com/debuggeando_ideas/real_appplications/ComparatorLambdas.java
+++ src/main/java/com/debuggeando_ideas/real_appplications/ComparatorLambdas.java
@@ -1,9 +1,7 @@
-package com.debuggeando_ideas.temp;
+package com.debuggeando_ideas.real_appplications;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import util.Database;
-import util.Videogame;
 
 import java.util.Arrays;
 import java.util.Comparator;
@@ -13,27 +11,23 @@ public class ComparatorLambdas {
 
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(4,3,6,8,7,5,8,10);
-
         numbers.sort((a, b) -> a - b);
-
         System.out.println(numbers);
-
         List<String> names = Arrays.asList( "Max", "Alex", "Opal");
-
         names.sort(String::compareTo);
-
         System.out.println(names);
-
         names.sort(Comparator.reverseOrder());
-
         System.out.println(names);
 
-        List<Person> persons = Arrays.asList( new Person("Robert", 31), new Person("Andy", 32), new Person("Petter", 27));
+        List<Person> persons = Arrays.asList(
+                new Person("Robert", 31),
+                new Person("Ben", 32),
+                new Person("Andy", 32),
+                new Person("Petter", 27));
 
-        persons.sort(Comparator.comparingInt(Person::getAge).thenComparing(Person::getAge));
+        persons.sort(Comparator.comparingInt(Person::getAge).thenComparing(Person::getName));
 
         System.out.println(persons);
-
     }
 }
 
--- src/main/java/com/debuggeando_ideas/temp/StreamIntermediateOperatorsT.java
+++ src/main/java/com/debuggeando_ideas/temp/StreamIntermediateOperatorsT.java
@@ -1,8 +1,11 @@
 package com.debuggeando_ideas.temp;
 
+import util.BasicVideogame;
 import util.Database;
+import util.Review;
 import util.Videogame;
 
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -11,7 +14,7 @@ public class StreamIntermediateOperatorsT {
     public static void main(String[] args) {
         Stream<Videogame> videogames = Database.videogames.stream();
 
-        filterOperator(videogames);
+        flatMapVSMapOperator(videogames);
     }
 
     static void distinctOperator(Stream<Videogame> stream) {
@@ -28,5 +31,30 @@ public class StreamIntermediateOperatorsT {
 
     static void filterOperator(Stream<Videogame> stream) {
         stream.filter(Videogame::getIsDiscount).collect(Collectors.toList()).forEach(System.out::println);
+    }
+
+    static void mapOperator(Stream<Videogame> stream) {
+        stream.map( v -> {
+            return BasicVideogame.builder()
+                    .name(v.getName())
+                    .price(v.getPrice())
+                    .console(v.getConsole())
+                    .build();
+        }).forEach(System.out::println);
+    }
+
+    static void flatMapOperator(Stream<Videogame> stream) {
+        List<Review> reviews = stream.flatMap(v -> v.getReviews().stream()).collect(Collectors.toList());
+
+        reviews.forEach(System.out::println);
+    }
+
+    static void flatMapVSMapOperator(Stream<Videogame> stream) {
+        //Long totalFlatMap = stream.flatMap(v -> v.getReviews().stream()).count();
+        Long totalMap = stream.map(v -> v.getTotalSold()).count();
+        //
+        // System.out.println(totalFlatMap);
+        System.out.println(totalMap);
+
     }
 }
